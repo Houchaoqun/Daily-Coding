8ecf6aa53d6ac5c4614a22084ba008c4d793abf6
houchaoqun <836999690@qq.com>
2017-05-13 22:52:48 +0800

    remove it web development

--- SettingFormula_django_highcharts_js/app_SettingFormula/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from app_SettingFormula.models import *
-
-# Register your models here.
-class FormulaAdmin(admin.ModelAdmin):
-	pass
-
-admin.site.register(Formula, FormulaAdmin)
